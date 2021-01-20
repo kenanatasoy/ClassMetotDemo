7edfb8237486d30ceaef7fe789e45a67d452bfdb
kenanatasoy <35261110+kenanatasoy@users.noreply.github.com>
2021-01-20 20:53:31 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            if (musteri.Cinsiyet == "Erkek")
+            {
+                Console.WriteLine(musteri.Ad + " bey eklendi.");
+            }
+
+            else if (musteri.Cinsiyet == "Kadın")
+            {
+                Console.WriteLine(musteri.Ad + " hanım eklendi.");
+            }
+
+            else
+            {
+                Console.WriteLine(musteri.Ad + " eklendi.");
+            }
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            if (musteri.Cinsiyet == "Erkek")
+            {
+                Console.WriteLine(musteri.Ad + " bey silindi.");
+            }
+
+            else if (musteri.Cinsiyet == "Kadın")
+            {
+                Console.WriteLine(musteri.Ad + " hanım silindi.");
+            }
+
+            else
+            {
+                Console.WriteLine(musteri.Ad + " silindi.");
+            }
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("Müşteriler: ");
+
+            foreach (Musteri musteri in musteriler)
+            {
+                if (musteri.Cinsiyet == "Erkek")
+                {
+                    Console.WriteLine(musteri.Ad + " bey");
+                }
+
+                else if (musteri.Cinsiyet == "Kadın")
+                {
+                    Console.WriteLine(musteri.Ad + " hanım");
+                }
+
+                else
+                {
+                    Console.WriteLine(musteri.Ad);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri[] musteri = new Musteri[4];
+
+            musteri[0] = new Musteri();
+
+            musteri[0].Id = 1000;
+            musteri[0].Ad = "Ayşe";
+            musteri[0].Soyad = "Katırcı";
+            musteri[0].Cinsiyet = "Kadın";
+
+            musteri[1] = new Musteri();
+
+            musteri[1].Id = 1001;
+            musteri[1].Ad = "Kemal";
+            musteri[1].Soyad = "İnce";
+            musteri[1].Cinsiyet = "Erkek";
+
+            musteri[2] = new Musteri();
+
+            musteri[2].Id = 1002;
+            musteri[2].Ad = "Selma";
+            musteri[2].Soyad = "Demirci";
+            musteri[2].Cinsiyet = "Kadın";
+
+            musteri[3] = new Musteri();
+
+            musteri[3].Id = 1003;
+            musteri[3].Ad = "Murat";
+            musteri[3].Soyad = "Bozkıran";
+            musteri[3].Cinsiyet = "Erkek";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri[1]);
+
+            musteriManager.MusteriSil(musteri[2]);
+
+            musteriManager.MusteriListele(musteri);
+
+        }
+    }
+}
